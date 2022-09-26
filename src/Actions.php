c42395e7c4a0f1eb582d0465f08d22a237a068c3
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2022-09-26 22:28:23 +0100

    Add support for REST endpoints for actions

--- src/Actions.php
+++ src/Actions.php
@@ -11,6 +11,21 @@ class Actions
     {
         foreach ($this->_getActions() as $action) {
             if ($action instanceof ActionInterface) {
+                add_action('rest_api_init', function () use ($action) {
+                    register_rest_route('wpgatsbytghp/v1', $action->getActionCode(), [
+                        'methods' => 'POST',
+                        'callback' => function ($request) use ($action) {
+                            try {
+                                return rest_ensure_response(
+                                    call_user_func([$action, 'executeNoPriv'])
+                                );
+                            } catch (\Exception $e) {
+                                return new \WP_Error('wpgatsbytghp_error', $e->getMessage(), [ 'status' => 503 ]);
+                            }
+                        },
+                    ]);
+                });
+
                 add_action('admin_post_' . $action->getActionCode(), function () use ($action) {
                     try {
                         $return = call_user_func([$action, 'executePriv']);
@@ -21,6 +36,7 @@ class Actions
                         exit(json_encode([ 'success' => false, 'message' => $e->getMessage() ]));
                     }
                 });
+
                 add_action('admin_post_nopriv_' . $action->getActionCode(), function () use ($action) {
                     try {
                         $return = call_user_func([$action, 'executeNoPriv']);
